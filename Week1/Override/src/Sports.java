b81ca96dbdc2838a82b7f8293939a6d11ebf7e9c
Bikram Singh <44418218+bikram9-9@users.noreply.github.com>
2020-04-17 16:47:24 -0500

    Add files via upload

---
+++ Week1/Override/src/Sports.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+  
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
